0c50d3e9980a8ae15c17b48f5df1103a81d781eb
U-SEMAX\florian.hofmaier <florian.hofmaier@smx-nb10-028.semax.local>
2017-08-18 09:52:42 +0200

    improve timing, better structure

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,39 +1,44 @@
 import sys, subprocess
 
+from operator import itemgetter
 from datetime import datetime
 from _datetime import date
 
 
 # input data ------------------------------------------------------------------
-p1_log = 'p1_170811.txt'
-rs485_log = 'RS485_170811.txt'
-
+p1_log = 'p1_170816.txt'
+rs485_log = 'RS485_170816.txt'
+input_list = [p1_log, rs485_log]
 
 # output data -----------------------------------------------------------------
 p1_int_output = 'p1.int.xx.txt'
 p1_timeStamp_output = 'p1.ts.xx.csv'
-
+p1_irregular_lenght_output = 'p1.lenght.changed.xx.txt'
+p1_missing_value_output = 'p1.missing.val.xx.txt'
+debug_output = 'debug.xx.txt'
 
 # settings --------------------------------------------------------------------
 serial_numbers = ['20237169']
 p1_int_limit = 0.5
 p1_int_time = 10
-date_format = '%Y-%m-%d %H:%M:%S.%f'
+date_format = '%Y-%m-%d %H:%M:%S.%f'    # don't dare to change this!! ;-)
 
 
 # file logger  ----------------------------------------------------------------
 # prints to stdout and logfile at the same time
 class Logger(object):
-    def __init__(self, file_name):
+    def __init__(self, file_name, verbose=False):
+        self.verbose = verbose
         self.terminal = sys.stdout
         self.log = open(file_name, "w")
 
     def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)  
+        if self.verbose:
+            self.terminal.write(message)
+        self.log.write(message)
 
     def flush(self):
-        pass    
+        pass
 
 
 # global functions ------------------------------------------------------------
@@ -50,72 +55,78 @@ def conv_time(t):
     return s
 
 
-# read p1 messages ------------------------------------------------------------
+# read input files  -----------------------------------------------------------
 print('read input files..', end='')
 sys.stdout.flush()
-def get_compose_time(lines):
-    time = 'compose time not found'
-    for line in lines:
-        if '0-0:1.0.0' in line:
-            time = line[37:49]
-    return time
 
-f = open(p1_log, 'r')
 p1_messages = []
-msg = {'lines': [],
-       'start': '',
-       'end': '',
-       'compose_time': ''}
-
-for line in f.readlines(): 
-    if 'ELS5' in line:
-        msg = {'start': get_time_stamp(line),
-               'lines': []}
-    if (len(line) == 37 and line[27] == '!'):
-        msg['end'] = get_time_stamp(line)
-        msg['compose_time'] = get_compose_time(msg['lines'])
-        p1_messages.append(msg)
-    msg['lines'].append(line)
-
-
-# read meter readouts from RS485 log ------------------------------------------
-f = open(rs485_log, 'r')
 sys_readouts = []
 meter_readouts = []
-data = {'lines': [],
-        'start': ''} 
 
-        
+
 def create_new_dataset(line, data, cat):        
     # bkp an allready open dataset
     data['end'] = get_time_stamp(line)
-    if cat == 'meter_readout':
+    if cat == 'mtr_readout':
         meter_readouts.append(data)
     if cat == 'sys_readout':
         sys_readouts.append(data)
+    if cat == 'p1_message':
+        p1_messages.append(data)
+            
     # create a new dataset
     return {'lines': [],
             'start': get_time_stamp(line)}
 
-cat = 'none'
-for line in f.readlines():
-    
-    # meter readout starts
-    if '/2!' in line:
-        # bkp open dataset and create a new one
-        data = create_new_dataset(line, data, cat)
-        cat = 'meter_readout'
-    # system readout starts
-    if any(sn + '!' in line for sn in serial_numbers):
-        # bkp open dataset and create a new one
-        data = create_new_dataset(line, data, cat)
-        cat = 'sys_readout'
-        
-    data['lines'].append(line)
 
+for filename in input_list:
+    
+    f = open(filename, 'r')
+    # create first dataset
+    data = {'lines': [],
+            'start': ''}
+    
+    cat = 'undefined'
+    for line in f.readlines():
+
+        # meter readout starts (read service list)
+        if '/2!\\r\\n' in line:
+            # bkp open dataset and create a new one
+            data = create_new_dataset(line, data, cat)
+            cat = 'mtr_readout'
+            data['cat'] = cat
+        # system readout starts (meter is addressed by serial)
+        if any(sn + '!' in line for sn in serial_numbers):
+            # bkp open dataset and create a new one
+            data = create_new_dataset(line, data, cat)
+            cat = 'sys_readout'
+            data['cat'] = cat
+        # p1 message starts (parse 'ELS5')
+        #if 'ELS5' in line and len(line) == 37:
+        if 'ELS5\\r\\n' in line:
+            # bkp open dataset and create a new one
+            data = create_new_dataset(line, data, cat)
+            cat = 'p1_message'
+            data['cat'] = cat
+            
+        data['lines'].append(line)
+    
 print('done')
 
+
 # analyse data ----------------------------------------------------------------
+
+print('indexing messages..', end='')
+sys.stdout.flush()  
+unsorted = [m for m in p1_messages] + [m for m in meter_readouts] + \
+           [m for m in sys_readouts]
+
+timeLine = sorted(unsorted, key=itemgetter('start'))
+for i, m in enumerate(timeLine):
+    m['idx_timeline'] = i
+print('done')
+
+
 
 def get_t_diff(m0, m1):
     t0 = datetime.strptime(m0['start'], date_format)
@@ -124,7 +135,19 @@ def get_t_diff(m0, m1):
     return float(str(diff.seconds)+'.'+str(diff.microseconds))
     
     
-# calc intervals 
+# get compose time for p1 messages
+print('read compose time for p1 messages..', end='')    
+sys.stdout.flush()
+time = 'compose time not found'
+for m in p1_messages:
+    for line in m['lines']:
+        if '0-0:1.0.0' in line:
+            time = line[37:49]
+        m['compose_time'] = time
+print('done')
+
+    
+# calc p1 intervals 
 print('calc p1 intervals..', end='')
 sys.stdout.flush()
 for i  in range(1, len(p1_messages)):
@@ -133,39 +156,65 @@ for i  in range(1, len(p1_messages)):
 print('done')
 
 
-# find bad intervals
+# find bad p1 intervals
 print('find bad intervals..', end='')
 sys.stdout.flush()
 p1_int_errors = []
 error = p1_int_time * p1_int_limit
 upper_limit = p1_int_time + error
 lower_limit = p1_int_time - error
-for i, m in enumerate(p1_messages):
-    try:
-        if m['t_int'] < lower_limit or m['_int'] > upper_limit:
-            m['last_push'] = p1_messages[i-1]
-            p1_int_errors.append(m)
-    except KeyError:
-        pass
+for i, m in enumerate(p1_messages[1:]):
+    if m['t_int'] < lower_limit or m['t_int'] > upper_limit:
+        m['last_push'] = p1_messages[i-1]
+        p1_int_errors.append(m)
 print('done')
-    
-    
+
+
+
+def build_msg_relations(l):
+# run through a given list of p1 messages and link nearby sys- and mtr-readouts
+    for m in (l):
+        if m['cat'] == 'p1_message':
+            sys_readout_before_found = False
+            mtr_readout_before_found = False
+            sys_readout_after_found = False
+            mtr_readout_after_found = False
+            # search backward
+            idx = m['idx_timeline']
+            i = idx
+            while not(sys_readout_before_found \
+            and mtr_readout_before_found) and i:
+                msg_check = timeLine[i-1]
+                if msg_check['cat'] == 'sys_readout' and sys_readouts \
+                and not sys_readout_before_found:
+                    m['sys_readout_before'] = msg_check
+                    sys_readout_before_found = True
+                if msg_check['cat'] == 'mtr_readout' and meter_readouts \
+                and not mtr_readout_before_found:
+                    m['mtr_readout_before'] = msg_check
+                    mtr_readout_before_found = True
+                i -= 1
+            # search foreward
+            i = idx
+            while not(sys_readout_after_found and mtr_readout_after_found) \
+            and i < len(timeLine)-1:
+                msg_check = timeLine[i+1]
+                if msg_check['cat'] == 'sys_readout' and sys_readouts \
+                and not sys_readout_after_found:
+                    m['sys_readout_after'] = msg_check
+                    sys_readout_after_found = True
+                if msg_check['cat'] == 'mtr_readout' and meter_readouts \
+                and not mtr_readout_after_found:
+                    m['mtr_readout_after'] = msg_check
+                    mtr_readout_after_found = True
+                i += 1
+
+
+
 # find sys readouts relating to p1 interval errors
 print('find sys readouts relating to p1 interval errors..', end='')
 sys.stdout.flush()
-for m in p1_int_errors:
-    m['sys_com_before'] = sys_readouts[0]
-    m['sys_com_after'] = sys_readouts[-1]
-    for readout in sys_readouts:
-        # find sys readout next to message
-        # before
-
-        if get_t_diff(readout, m) < get_t_diff(m['sys_com_before'], m):
-            m['sys_com_before'] = readout
-
-        # after
-        if get_t_diff(m, readout) > get_t_diff(m['sys_com_after'], m):
-            m['sys_com_after'] = readout
+build_msg_relations(p1_int_errors)
 print('done')
 
 
@@ -180,40 +229,100 @@ for m in p1_messages:
 print('done')
 
 
+# analyze p1 message lenght
+print('check lenght of p1 messages..', end='')
+sys.stdout.flush()
+for i, m in enumerate(p1_messages[1:]):
+    last_message = p1_messages[i-1]
+    if len(m['lines']) != len(last_message['lines']):
+        m['lenght_changed'] = True
+    len(m['lines'])
+print('done')
+
+
+# search missing p1 values
+print('search for missing p1 values..', end='')
+sys.stdout.flush()
+for m in p1_messages:
+    for l in m['lines']:
+        if '()' in l:
+            m['missing_value'] = True
+print('done')
+
+
+def print_msg_relations(msg):
+    t = m['compose_time']
+    print('    time-stamp push:      %s (%s)' %(conv_time(t), t))
+    
+    print('    time push:            %s - %s' %(m['start'], m['end']))
+    print('    time last push:       %s - %s' %(m['last_push']['start'],
+                                                m['last_push']['end']))
+  
+    try:
+        print('    sys readout before:   %s - %s' %(m['sys_readout_before']['start'],
+                                                    m['sys_readout_before']['end']))
+    except KeyError:
+        print('    sys readout before: none')
+    
+    try:
+        print('    sys readout after:    %s - %s' %(m['sys_readout_after']['start'],
+                                                    m['sys_readout_after']['end']))
+    except KeyError:
+        print('    sys readout after:')
+    
+    try:
+        print('    meter readout before: %s - %s' %(m['mtr_readout_before']['start'],
+                                                    m['mtr_readout_before']['end']))
+    except KeyError:
+        print('    meter readout before:')
+    
+    try:
+        print('    meter readout after:  %s - %s' %(m['mtr_readout_after']['start'],
+                                                    m['mtr_readout_after']['end']))
+    except KeyError:
+        print('    meter readout after:')
+
+    print()
+
+'''
+# write debug output ----------------------------------------------------
+output0 = debug_output.replace('xx', datetime.now().strftime('%Y%m%d%H%M')[2:])
+print('\nprint debug info to %s..' %output0)
+# redirect stdout
+sys.stdout = Logger(output0, verbose=False)
+print('debug info:')
+for m in p1_int_errors:
+    print(m['start'])
+    print('    ', m['mtr_readout_before'])
+sys.stdout = sys.__stdout__
+print('done')
+subprocess.Popen('notepad.exe %s' %output0)
+
+'''
+
+
 # write p1 interval output ----------------------------------------------------
 output1 = p1_int_output.replace('xx', datetime.now().strftime('%Y%m%d%H%M')[2:])
-print('\nprint p1 interval data to %s:' %output1)
+print('\nprint p1 interval data to %s..' %output1)
 # redirect stdout
-sys.stdout = Logger(output1)
+sys.stdout = Logger(output1, verbose=False)
 print('%i p1 messages and %i system readouts analysed..' %(len(p1_messages),
                                                            len(sys_readouts)))
 print('%i time interval(s) out of tolerance (+/-%i%%):' 
       %(len(p1_int_errors), int(p1_int_limit * 100)))
 for i, m in enumerate(p1_int_errors):
-    print('\n%f' %(m['t_int']))
-    print('    time push:           %s - %s' %(m['start'], m['end']))
-    print('    time last push:      %s - %s' %(m['last_push']['start'],
-                                                m['last_push']['end']))
-    t = m['compose_time']
-    print('    time stamp push:     %s (%s)' %(conv_time(t), t))
-    #print('    msg compose time:   %s' %conv_time(m['compose_time']))
-    print('    sys readout before:  %s - %s' %(m['sys_com_before']['start'],
-                                              m['sys_com_before']['end']))
-    print('    sys readout during: ', end='')
-    for i in m['sys_com_during']:
-        print('%s, ' %i['start'], end='')
-    print()
-    print('    sys readout after:   %s' %m['sys_com_after']['start'])
-
+    print('\nbad interval (%fs):' %(m['t_int']))
+    print_msg_relations(m)
 sys.stdout = sys.__stdout__
+print('done')
+subprocess.Popen('notepad.exe %s' %output1)
 
-
+'''
 # write p1 timeStamp output ---------------------------------------------------
 output2 = p1_timeStamp_output.replace('xx', datetime.now().strftime('%Y%m%d%H%M')[2:])
-print('\nprint p1 interval data to %s:' %output1)
+print('\nprint p1 interval data to %s..' %output1)
 # redirect stdout
-sys.stdout = Logger(output2)
-
+sys.stdout = Logger(output2, verbose=False)
 print('time;timeStamp;timeStamp_converted;diff;')
 for m in p1_messages:
     t0 = datetime.strptime(m['start'][:19], '%Y-%m-%d %H:%M:%S')
@@ -225,8 +334,48 @@ for m in p1_messages:
                         conv_time(m['compose_time']), diff))
 
 sys.stdout = sys.__stdout__
-
-
-# open files in editor --------------------------------------------------------
-subprocess.Popen('notepad.exe %s' %output1)
+print('done')
 subprocess.Popen('notepad.exe %s' %output2)
+'''
+
+# write irregular messages lenght ouput ---------------------------------------
+output3 = p1_irregular_lenght_output.replace('xx', datetime.now().strftime('%Y%m%d%H%M')[2:])
+print('\nprint missing p1 values information to %s..' %output3)
+# redirect stdout
+sys.stdout = Logger(output3, verbose=False)
+cnt = 0
+for m in p1_messages:
+    try:
+        if m['lenght_changed']:
+            print('%s lenght changed to %i lines' %(m['start'], len(m['lines'])))
+            cnt += 1
+    except KeyError:
+        pass
+if cnt == 0:
+    print('all logged p1 messages have the same lenght (%i lines)' %len(m['lines']))
+sys.stdout = sys.__stdout__
+print('done')
+subprocess.Popen('notepad.exe %s' %output3)
+
+'''
+# write missing values ouput ---------------------------------------
+output4 = p1_missing_value_output.replace('xx', datetime.now().strftime('%Y%m%d%H%M')[2:])
+print('\nprint p1 interval data to %s..' %output4)
+# redirect stdout
+sys.stdout = Logger(output4, verbose=False)
+cnt = 0
+for m in p1_messages:
+    try:
+        if m['missing_value']:
+            print('missing value(s):')
+            print('    %s' %(m['start']))
+            cnt += 1
+    except KeyError:
+        pass
+if cnt == 0:
+    print('no missing values detected..')
+sys.stdout = sys.__stdout__
+print('done')
+subprocess.Popen('notepad.exe %s' %output4)
+
+'''
